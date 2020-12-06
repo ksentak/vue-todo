c3dc1221f0cf8fcf374672de8e4b9eb78d320c7f
Keaton Sentak <k.sentak@outlook.com>
2020-12-06 12:40:44 -0500

    add vue-router

---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the Vue-Todo app v1.0.0. I built this to begin learning Vue.</p>
+  </div>
+</template>
+
+<style scoped>
+  .about {
+    padding: 20px;
+  }
+</style>
