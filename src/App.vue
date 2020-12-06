92f7a50daa21a280e90a7f2bdd9a5bc89555bd86
Keaton Sentak <k.sentak@outlook.com>
2020-12-06 11:00:06 -0500

    move to vue2 and create todos component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,53 @@
 <template>
-  <h1>Vue 3</h1>
+  <div id="app">
+    <Todos/>
+  </div>
 </template>
 
 <script>
+import Todos from './components/Todos'
 
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Learn Vue',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Optimize portfolio',
+          completed: false
+        },
+        {
+          id: 3,
+          title: 'Do laundry',
+          completed: false
+        }
+      ]
+    }
+  }
+  
 }
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Space+Mono&display=swap');
-#app {
-  font-family: 'Space Mono', monospace;
+@import url('https://fonts.googleapis.com/css2?family=Rubik&display=swap');
+
+* {
+  box-sizing: border-box;
   margin: 0;
+  padding:0;
+}
+
+body {
+  font-family: 'Rubik', sans-serif;
+  line-height: 1.4;
 }
 </style>
